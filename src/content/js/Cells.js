10671ea288144b80beae308ddda9bfe5123917c0
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2011-08-07 13:43:05 +0200

    Cell getted from the server

--- src/content/js/Cells.js
+++ src/content/js/Cells.js
@@ -1,16 +1,43 @@
-var Map = Class.create(
+var Cells = Class.create(
 {
-	initialize: function(element, columns,rows, cellW,cellH)
+	initialize: function(element, rows,columns, cellW,cellH)
 	{
 //		element.draggable();
 
 		this.element = element;
 
-		this.columns = columns;
 		this.rows = rows;
+		this.columns = columns;
 
 		this.cellW = cellW;
 		this.cellH = cellH;
+	},
+
+	load: function(data)
+	{
+		var cellH = data.cells.length;
+		var cellW = data.cells[0].length;
+
+		for(var y=0; y<cellH; y++)
+			for(var x=0; x<cellW; x++)
+			{
+				var tile = null;
+
+				var cell = data.cells[y][x];
+				if(cell < 0) continue;
+
+				switch(cell)
+				{
+					case 1:	tile = new Tile("content/tiles/house.png", 2,2);			break;
+					case 2:	tile = new Tile("content/tiles/building-medium.png", 3,3);	break;
+					case 3:	tile = new Tile("content/tiles/building-generic.png", 4,4);	break;
+				};
+
+				if(!tile)
+					tile = new Tile("content/tiles/grass.png", 1,1);
+
+				this.put(tile, x,y);
+			}
 	},
 
 	put: function(tile, coorX,coorY)
@@ -40,7 +67,7 @@ var Map = Class.create(
 
 			// Remove old tile, put new one, set tool to null and disable it if we can't be able to use it anymore
 			parent.removeChild(this);
-			map.put(new Tile("content/tiles/house.png", 2,2), coorX,coorY);
+			map.put(new Tile("content/tiles/"+currentTool+".png", 2,2), coorX,coorY);
 			currentTool = null;
 		}
 	}
